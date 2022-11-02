53538328537439753d555cba64c8ab89eed6068a
random-geek <35757396+random-geek@users.noreply.github.com>
2022-11-02 13:17:58 -0700

    Update delete_objects to work with new Lua serialization format

--- src/commands/delete_objects.rs
+++ src/commands/delete_objects.rs
@@ -9,7 +9,15 @@ use crate::utils::{query_keys, to_bytes, to_slice, fmt_big_num};
 use memmem::{Searcher, TwoWaySearcher};
 
 const ITEM_ENT_NAME: &[u8] = b"__builtin:item";
-const ITEM_NAME_PAT: &[u8] = b"[\"itemstring\"] = \"";
+const ITEM_NAME_PAT_OLD: &[u8] = b"[\"itemstring\"] = \"";
+const ITEM_NAME_PAT_NEW: &[u8] = b"itemstring=\"";
+
+thread_local! {
+	static ITEM_NAME_SEARCHER_OLD: TwoWaySearcher<'static> =
+		TwoWaySearcher::new(ITEM_NAME_PAT_OLD);
+	static ITEM_NAME_SEARCHER_NEW: TwoWaySearcher<'static> =
+		TwoWaySearcher::new(ITEM_NAME_PAT_NEW);
+}
 
 
 fn verify_args(args: &InstArgs) -> ArgResult {
@@ -21,11 +29,23 @@ fn verify_args(args: &InstArgs) -> ArgResult {
 
 
 #[inline]
-fn get_item_name<'a>(data: &'a [u8], searcher: &TwoWaySearcher) -> &'a[u8] {
+fn get_item_name_start(data: &[u8]) -> Option<usize> {
+	if let Some(idx) = ITEM_NAME_SEARCHER_NEW.with(|s| s.search_in(data)) {
+		Some(idx + ITEM_NAME_PAT_NEW.len())
+	} else if let Some(idx) = ITEM_NAME_SEARCHER_OLD.with(|s| s.search_in(data)) {
+		Some(idx + ITEM_NAME_PAT_OLD.len())
+	} else {
+		None
+	}
+}
+
+
+#[inline]
+fn get_item_name<'a>(data: &'a [u8]) -> &'a[u8] {
 	if data.starts_with(b"return") {
-		if let Some(idx) = searcher.search_in(data) {
-			let name = &data[idx + ITEM_NAME_PAT.len()..]
-				.split(|&c| c == b' ' || c == b'"').next();
+		let item_name_start = get_item_name_start(data);
+		if let Some(idx) = item_name_start {
+			let name = &data[idx..].split(|&c| c == b' ' || c == b'"').next();
 			if let Some(n) = name {
 				return n;
 			}
@@ -42,8 +62,7 @@ fn can_delete(
 	area: &Option<Area>,
 	invert: bool,
 	obj_name: &Option<Vec<u8>>,
-	item_names: &[Vec<u8>],
-	item_name_searcher: &TwoWaySearcher
+	item_names: &[Vec<u8>]
 ) -> bool {
 	// Check area requirement
 	if let Some(a) = area {
@@ -63,8 +82,7 @@ fn can_delete(
 			}
 
 			if !item_names.is_empty() {
-				let item_name =
-					get_item_name(&le_data.data, &item_name_searcher);
+				let item_name = get_item_name(&le_data.data);
 				if !item_names.iter().any(|n| n == item_name) {
 					// Item entity's item name does not match.
 					return false
@@ -88,7 +106,6 @@ fn delete_objects(inst: &mut InstBundle) {
 
 	let item_names: Vec<_> = inst.args.items.as_ref().unwrap_or(&Vec::new())
 		.iter().map(to_bytes).collect();
-	let item_name_searcher = TwoWaySearcher::new(ITEM_NAME_PAT);
 
 	let keys = query_keys(&mut inst.db, &mut inst.status,
 		to_slice(&obj_name), inst.args.area, inst.args.invert, true);
@@ -108,8 +125,7 @@ fn delete_objects(inst: &mut InstBundle) {
 				&inst.args.area,
 				inst.args.invert,
 				&obj_name,
-				&item_names,
-				&item_name_searcher
+				&item_names
 			) {
 				block.static_objects.remove(i);
 				modified = true;
@@ -150,7 +166,6 @@ mod tests {
 
 	#[test]
 	fn test_delete_objects() {
-		let searcher = TwoWaySearcher::new(ITEM_NAME_PAT);
 		let pairs: &[(&[u8], &[u8])] = &[
 			(b"default:glass", b"default:glass"),
 			(b"return {}", b""),
@@ -158,9 +173,12 @@ mod tests {
 			(b"return {[\"itemstring\"] = \"mod:item\"}", b"mod:item"),
 			(b"return {[\"age\"] = 400, [\"itemstring\"] = \"one:two 99 32\"}",
 				b"one:two"),
+			(b"return {itemstring=\"\",age=100}", b""),
+			(b"return {itemstring=\"mod:item\"}", b"mod:item"),
+			(b"return {age=400,itemstring=\"one:two 99 32\"}", b"one:two"),
 		];
 		for &(data, name) in pairs {
-			assert_eq!(get_item_name(data, &searcher), name);
+			assert_eq!(get_item_name(data), name);
 		}
 	}
 }
